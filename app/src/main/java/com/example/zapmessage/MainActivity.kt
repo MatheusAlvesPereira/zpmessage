4c433d2a012bffe3c09ba6c1a6427d5e647edcf8
Matheus Alves <matheusalvessilvabr@gmail.com>
2025-10-21 17:12:57 -0300

    adding input and navigation system

--- app/src/main/java/com/example/zapmessage/MainActivity.kt
+++ app/src/main/java/com/example/zapmessage/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
@@ -19,10 +18,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.zapmessage.ui.theme.ZapmessageTheme
 
 class MainActivity : ComponentActivity() {
@@ -58,7 +58,31 @@ fun ZapmessageApp() {
                 )
             }
         }
-    )
+    ) {
+        when (currentDestination) {
+            AppDestinations.HOME -> {
+                AndroidView(
+                    modifier = Modifier.fillMaxSize(),
+                    factory = { context ->
+                        android.view.LayoutInflater.from(context)
+                            .inflate(R.layout.mainlayout, null)
+                    }
+                )
+            }
+
+            AppDestinations.FAVORITES -> {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text("Favorites Screen")
+                }
+            }
+
+            AppDestinations.PROFILE -> {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text("Profile Screen")
+                }
+            }
+        }
+    }
 }
 
 enum class AppDestinations(
---
+++ app/src/main/java/com/example/zapmessage/SearchBar.kt
@@ -0,0 +1,5 @@
+package com.example.zapmessage
+
+fun SearchBar() {
+
+}
